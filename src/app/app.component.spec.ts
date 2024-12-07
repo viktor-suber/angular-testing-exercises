e644ef8962fc9f65112a002d338dd9a8a3261f0b
Viktor Suber <viktor.suber@gmail.com>
2024-12-07 14:33:13 -0500

    initialize

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,79 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled: HTMLElement;
+  let app: AppComponent;
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
-  });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'angular-testing-exercises' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-testing-exercises');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
+    compiled = fixture.nativeElement as HTMLElement;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-testing-exercises');
   });
+
+  describe('Initialization and Layout', () => {
+    beforeEach(() => {
+      app = fixture.componentInstance;
+    });
+
+    it('should create the app', () => {
+      expect(app).toBeTruthy();
+    });
+  
+    it(`should have the 'angular-testing-exercises' title`, () => {
+      expect(app.title).toEqual('angular-testing-exercises');
+    });
+
+    it(`should render a <main> element with class 'main' that contains a <div> with class 'outer-content'`, () => {
+      const mainElement = compiled.querySelector('main');
+      expect(mainElement).toBeTruthy();
+      expect(mainElement?.classList).toContain('main');
+      const outerContentDiv = mainElement?.querySelector('div.outer-content');
+      expect(outerContentDiv).toBeTruthy();
+    });
+
+    it(`should render a <div> with class 'outer-content' that contains a <div> with class 'inner-content' and two <div> with class 'divider'`, () => {
+      const outerContentDiv = compiled.querySelector('div.outer-content');
+      expect(outerContentDiv).toBeTruthy();
+      const innerContentDiv = outerContentDiv?.querySelector('div.inner-content');
+      expect(innerContentDiv).toBeTruthy();
+      const dividers = outerContentDiv?.querySelectorAll('div.divider');
+      expect(dividers?.length).toEqual(2);
+    });
+  });
+
+  describe('Header elements', () => {
+    it('should render the Angular logo', () => {
+      const ngLogoComponentElement = compiled.querySelector('app-angular-logo');
+      expect(ngLogoComponentElement).toBeTruthy();
+    });
+  
+    it('should render the content of logo', () => {
+      const ngLogoSvg = compiled.querySelector('app-angular-logo svg');
+      expect(ngLogoSvg).toBeTruthy();
+    });
+  
+    it('should render title', () => {
+      expect(compiled.querySelector('h2')?.textContent).toContain('Hello, angular-testing-exercises');
+    });
+  
+    it('should render subtitle', () => {
+      expect(compiled.querySelector('h3')?.textContent).toContain('Angular unit testing exercise.');
+    });
+  });
+
+  describe('Body paragraph', () => {
+    it('should render paragraph with body text', () => {
+      const paragraph = compiled.querySelector('p.body-text');
+    
+      expect(paragraph).toBeTruthy();
+      expect(paragraph?.textContent).toContain('Congratulations! Your app is running.');
+    });
+  });
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AngularLogoComponent } from "./angular-logo/angular-logo.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AngularLogoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
